5485852144eb0121f75863044fe9980a4941706e
Aabhash Rana <aabhashbikramrana@gmail.com>
2023-10-11 17:16:57 -0500

    formatting changes

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -4,7 +4,6 @@ const { addIncome, getIncomes, deleteIncome } = require('../controllers/income')
 const router = require('express').Router();
 
 
-
 router.post('/add-income', addIncome)
       .get('/get-incomes', getIncomes)
       .delete('/delete-income/:id', deleteIncome)
