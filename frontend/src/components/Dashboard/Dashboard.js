689e33211bf4a21abbe91b864900f4d38450360f
Aabhash Rana <aabhashbikramrana@gmail.com>
2023-10-30 19:02:19 -0500

    Dashboard Data Analysis Chart

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,45 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-
+import { useGlobalContext } from '../../context/globalContext';
+import { InnerLayout } from '../../styles/Layouts';
+import { dollar } from '../../utils/Icons';
+import Chart from '../Chart/Chart';
 
 function Dashboard() {
-    
+    const {totalExpenses,incomes, expenses, totalIncome, totalBalance, getIncomes, getExpenses } = useGlobalContext()
+
+    useEffect(() => {
+        getIncomes()
+        getExpenses()
+    }, [])
+
     return (
-        <DashboardStyled> Dashboard </DashboardStyled>
+        <DashboardStyled>
+            <InnerLayout>
+                <h1>All Transactions</h1>
+                <div className="stats-con">
+                    <div className="chart-con">
+                        <Chart />
+                        <div className="amount-con">
+                            <div className="income">
+                                <h2>Total Income</h2>
+                                <p>
+                                    {dollar} {totalIncome()}
+                                </p>
+                            </div>
+                            <div className="expense">
+                                <h2>Total Expense</h2>
+                                <p>
+                                    {dollar} {totalExpenses()}
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+            </InnerLayout>
+        </DashboardStyled>
     )
 }
 
